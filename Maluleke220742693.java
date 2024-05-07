5f331506c548fd0611e45b20a760b6a5793c87d3
Risima <220742693@ump.ac.za>
2024-05-07 15:06:22 +0200

    First commit for test2

---
+++ Maluleke220742693.java
@@ -0,0 +1,12 @@
+public class Maluleke220742693 {
+
+    public static void main(String[] args) {
+
+        int num1 = 18;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
